b61ac12bd0901d36c8d5ac6800ba651eb7267136
litoo <excute2015@outlook.com>
2015-11-02 20:10:34 +0800

    base

---
+++ app/src/main/java/com/example/administrator/piankeapp/BaseFragment.java
@@ -0,0 +1,14 @@
+package com.example.administrator.piankeapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/11/2.
+ */
+public class BaseFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
